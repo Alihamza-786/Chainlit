c26fe75cd265a7f552b04e64f201002d996399dd
Ali Hamza <alihamxa5412@gmail.com>
2025-10-06 15:27:13 +0500

    [Add] Customize theme in Chianlit

--- 1.cl.on_chat_start Print Welcome/app.py
+++ 1.cl.on_chat_start Print Welcome/app.py
@@ -1,11 +1,10 @@
 import os
+import asyncio
 import chainlit as cl
 from typing import TypedDict, List
 from langgraph.graph import StateGraph
 from langchain_ollama import ChatOllama
 from langchain_core.messages import HumanMessage, BaseMessage, AIMessage
-
-
 
 # Initialize LLM
 llm = ChatOllama(model="qwen2.5", streaming=True)
@@ -14,16 +13,33 @@ llm = ChatOllama(model="qwen2.5", streaming=True)
 class AgentState(TypedDict):
     messages: List[BaseMessage]
 
+# Separated streaming function
+async def stream_llm_response(messages: List[BaseMessage]) -> AIMessage:
+    msg = cl.Message(content="")
+    await msg.send()
+
+    content = ""
+    try:
+        async for chunk in llm.astream(messages):
+            if chunk.content:
+                content += chunk.content
+                await msg.stream_token(chunk.content)
+    except asyncio.CancelledError:
+        print("\n⚠️ Streaming was interrupted.")
+        pass
+
+    await msg.update()
+    return AIMessage(content=content)
 
 # LangGraph node
-def llm_node(state: AgentState) -> AgentState:
+async def llm_node(state: AgentState) -> AgentState:
 
-    response =llm.invoke(state["messages"])
-
+    response = await stream_llm_response(state["messages"])
     state["messages"].append(AIMessage(content=response.content))
 
-    print("\nState after llm: ", state)
+    print("\nFINAL STATE: ", state)
     return state
+
 
 # Build LangGraph
 graph = StateGraph(AgentState)
@@ -37,3 +53,24 @@ agent = graph.compile()
 async def on_chat_start():
     cl.user_session.set("state", {"messages": []})
     await cl.Message(content=f"👋 Hello! I'm your AI assistant. How can I help you today?").send()
+
+# Handle Messages
+@cl.on_message
+async def on_message(msg: cl.Message):
+
+    state = cl.user_session.get("state")
+    state["messages"].append(HumanMessage(content=msg.content))
+
+    final_state = await agent.ainvoke(state)
+
+    cl.user_session.set("state", final_state)
+
+# Chat Stop
+@cl.on_stop
+def on_stop():
+    print("The user wants to stop the task!")
+
+# Chat End
+@cl.on_chat_end
+def on_chat_end():
+    print("The user disconnected!")
---
+++ go Bar data go Bar y.md
@@ -0,0 +1,10 @@
+# Histogram
+data=[go.Histogram(y=[1, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4])],
+
+# Scatter
+data=[go.Scatter(x=['Jan', 'Feb', 'Mar', 'Apr', 'May'], y=[100, 105, 115, 110, 120])],
+
+# Pie
+data=[go.Pie(values=[10, 27, 63], labels=['Population', 'Empty Land', 'Water'])],
+
+
